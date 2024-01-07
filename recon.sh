bef873866773c411e2c93683d0c4826dfe5b1132
EdioPaul <ediopaulo.ep@outlook.com>
2024-01-07 10:41:17 -0300

    update

--- recon.sh
+++ recon.sh
@@ -8,24 +8,24 @@ mkdir $url
 cd $url
 echo $url | anew url;
 
-gau $(cat url) | grep "\.js" | /home/edio/go/bin/./httpx -mc 200 -silent  | anew saida.txt;
-xargs -a saida.txt -n2 -I{} bash -c "echo -e '\n[URL]: {}\n'; python3 /linkfinder.py -i {} -o cli" | python3 /collector.py output;
-
-cat saida.txt | python3 /collector.py;
-
-cat saida.txt | mantra | anew mantra;
-
-python3 /home/edio/JSScanner/./JSScanner.py;
+gau $(cat url) | grep "\.js" | /home/edio/go/bin/./httpx -mc 200 -silent  | anew gau;
+xargs -a gau -n2 -I{} bash -c "echo -e '\n[URL]: {}\n'; python3 /linkfinder.py -i {} -o cli" | python3 /collector.py output;
 
 subfinder -d $(cat url)   | /home/edio/go/bin/./httpx -silent -threads 1000 | xargs -I@ sh -c 'findomain -t @ -q | /home/edio/go/bin/./httpx -silent | anew | waybackurls | anew allurls';
-cat allurls | gf xss      | hakcheckurl | grep 200 | anew xss;
-cat allurls | gf lfi      | hakcheckurl | grep 200 | anew lfi;
-cat allurls | gf rce      | hakcheckurl | grep 200 | anew rce;
-cat allurls | gf sqli     | hakcheckurl | grep 200 | anew sqli;
-cat allurls | gf idor     | hakcheckurl | grep 200 | anew idor;
-cat allurls | gf ssrf     | hakcheckurl | grep 200 | anew ssrf;
-cat allurls | gf ssti     | hakcheckurl | grep 200 | anew ssti;
-cat allurls | gf redirect | hakcheckurl | grep 200 | anew redirect;
+cat allurls | gf xss      | anew xss;
+cat allurls | gf lfi      | anew lfi;
+cat allurls | gf rce      | anew rce;
+cat allurls | gf sqli     | anew sqli;
+cat allurls | gf idor     | anew idor;
+cat allurls | gf ssrf     | anew ssrf;
+cat allurls | gf ssti     | anew ssti;
+cat allurls | gf redirect | anew redirect;
+
+cat gau allurls | grep -E '\.(js|txt)$' | anew js
+
+cat js | mantra | anew mantra;
+
+python3 /home/edio/JSScanner/./JSScanner.py;
 
 subfinder -d $(cat url) -silent -all | /home/edio/go/bin/./httpx -silent -sc -t 1000 | anew subfinder;
 cat subfinder | grep 200 | anew http200;
@@ -48,7 +48,7 @@ curl -s "https://crt.sh/?q=$(cat url)&output=json" | jq -r '.[].name_value' | as
 
 whatweb $(cat url) | anew whatweb;
 
-nuclei -t /home/edio/fuzzing-templates -list saida.txt | anew fuzzingtemplates;
+nuclei -t /home/edio/fuzzing-templates -list gau | anew fuzzingtemplates;
 
 python3 /home/edio/dirsearch/dirsearch.py -u $(cat url);
 
@@ -66,8 +66,13 @@ echo $(cat url) | waybackurls | gf xss | uro | qsreplace '"><img src=x onerror=a
 gau $(cat url)  | /home/edio/go/bin/./httpx -silent | qsreplace '<script>alert(1)</script>' | freq | egrep -v 'Not' | anew xss2;
 echo $(cat url) | /home/edio/go/bin/./httpx -silent | katana -d 10 -silent | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' | anew xss3;
 echo $(cat url) | /home/edio/go/bin/./httpx -silent | hakrawler -subs | grep "=" | qsreplace '"><svg onload=confirm(1)>' | airixss -payload "confirm(1)" | egrep -v 'Not' | anew xss4;
-
 echo $(cat url) | /home/edio/go/bin/./httpx -silent | hakrawler -subs | grep "=" | qsreplace '{{7*7}}' | freq | egrep -v 'Not' | anew ssti2;
+
+cat xss | uro | qsreplace '"><img src=x onerror=alert(1);>'  | freq | egrep -v 'Not' | anew xss1.1;
+cat xss | qsreplace '<script>alert(1)</script>' | freq | egrep -v 'Not' | anew xss2.1;
+cat xss | uro | qsreplace '"><img src=x onerror=alert(1);>' | freq | egrep -v 'Not' | anew xss3.1;
+cat xss | qsreplace '"><svg onload=confirm(1)>' | airixss -payload "confirm(1)" | egrep -v 'Not' | anew xss4.1;
+cat ssti | qsreplace '{{7*7}}' | freq | egrep -v 'Not' | anew ssti2.1;
 
 echo $(cat url) | subfinder -silent | /home/edio/go/bin/./httpx -p 80,443,81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672 | nuclei -severity low,medium,high,critical | anew nuclei;
 
